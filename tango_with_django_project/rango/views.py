ca1bca0af5b66f5d062bd698daa2416e0df702fc
unknown <2089278r@BO706-04.ad.dcs.gla.ac.uk>
2015-01-22 19:03:24 +0000

    Chapter 5 - Completed

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -7,4 +7,5 @@ def index(request):
      return render(request, 'rango/index.html', context_dict)
 
 def about(request):
-    return HttpResponse("This tutorial has been put together by Stig Rasmus Renvall, 2089278")
+    context_dict = {'boldmessage': "This tutorial has been put together by Stig Rasmus Renvall, 2089278" }
+    return render(request, 'rango/about.html', context_dict)
