c3a00ff76ced327be429bba46bd8bd2b172afb26
unknown <2089278r@BO706-04.ad.dcs.gla.ac.uk>
2015-01-22 18:43:48 +0000

    Chapter 5 - a working rango page

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hello yet again!")
+     context_dict = {'boldmessage': "I am bold font from the context"}
+     return render(request, 'rango/index.html', context_dict)
+
 def about(request):
     return HttpResponse("This tutorial has been put together by Stig Rasmus Renvall, 2089278")
