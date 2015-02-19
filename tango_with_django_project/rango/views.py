cc167bea4715b508d9b5d8ecca0574e4d169b189
Stig Rasmus Renvall <2089278r@mail.student.gla.ac.uk>
2015-02-19 15:32:09 +0000

    Chapter 11 - Completed..?

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -5,16 +5,51 @@ from rango.models import Page
 from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from datetime import datetime
 
 def index(request):
-     category_list = Category.objects.order_by('likes')[:5]
-     page_list = Page.objects.order_by('views')[:5]
-     context_dict = {'categories': category_list, 'pages': page_list}
-     return render(request, 'rango/index.html', context_dict)
+
+    category_list = Category.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
+
+    context_dict = {'categories': category_list, 'pages': page_list}
+
+    visits = request.session.get('visits')
+    if not visits:
+        visits = 1
+    reset_last_visit_time = False
+
+    last_visit = request.session.get('last_visit')
+    if last_visit:
+        last_visit_time = datetime.strptime(last_visit[:-7], "%Y-%m-%d %H:%M:%S")
+
+        if (datetime.now() - last_visit_time).seconds > 0:
+            # ...reassign the value of the cookie to +1 of what it was before...
+            visits = visits + 1
+            # ...and update the last visit cookie, too.
+            reset_last_visit_time = True
+    else:
+        # Cookie last_visit doesn't exist, so create it to the current date/time.
+        reset_last_visit_time = True
+
+    if reset_last_visit_time:
+        request.session['last_visit'] = str(datetime.now())
+        request.session['visits'] = visits
+    context_dict['visits'] = visits
+
+
+    response = render(request,'rango/index.html', context_dict)
+
+    return response
 
 def about(request):
-    context_dict = {'boldmessage': "This tutorial has been put together by Stig Rasmus Renvall, 2089278" }
-    return render(request, 'rango/about.html', context_dict)
+    if request.session.get('visits'):
+        count = request.session.get('visits')
+    else:
+        count = 0
+    context_dict = {'boldmessage': "This tutorial has been put together by Stig Rasmus Renvall, 2089278"}
+
+    return render(request, 'rango/about.html', {'visits':count})
 
 def category(request, category_name_slug):
     context_dict = {}
@@ -86,6 +121,9 @@ def add_page(request, category_name_slug):
     return render(request, 'rango/add_page.html', context_dict)
 
 def register(request):
+    if request.session.test_cookie_worked():
+        print ">>>>> Test Cookie Worked !"
+        #request.session.delete_test_cookie()
 
     # A boolean value for telling the template whether the registration was successful.
     # Set to False initially. Code changes value to True when registration succeeds.
